27fa325f5b9ee8c3dd3d3164ee99beeeb019e6fb
Koji-0104 <90697671+Koji-0104@users.noreply.github.com>
2022-02-24 21:43:13 +0900

    Fix:インデントの修正

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe < ApplicationRecord
-	validates :name, uniqueness: { case_sensitive: true }, presence: true
-	validates :url, presence: true
-	validates :img_url, presence: true
+  validates :name, uniqueness: { case_sensitive: true }, presence: true
+  validates :url, presence: true
+  validates :img_url, presence: true
 end
