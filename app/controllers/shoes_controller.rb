65db19877f858859ef31bf24514a784d757d1fdb
Koji-0104 <90697671+Koji-0104@users.noreply.github.com>
2022-03-04 21:05:11 +0900

    Remove:不要なメソッドの削除

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -7,14 +7,4 @@ class ShoesController < ApplicationController
     @comment = Comment.new
     @comments = @shoe.comments.includes(:user).order(created_at: :desc)
   end
-
-  private
-
-  def find_shoe
-    @shoe = current_user.shoes.find(params[:id])
-  end
-
-  def shoe_params
-    params.require(:shoe).permit(:name, :url, :img_url)
-  end
 end
