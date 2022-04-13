aeba217598922985aa23b71672cacd441c007d89
Koji-0104 <90697671+Koji-0104@users.noreply.github.com>
2022-04-13 20:01:24 +0900

    Add:プライバシーポリシー、利用規約の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'home#index'
-
+  get 'privacy', to: 'home#privacy'
+  get 'terms', to: 'home#terms'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
